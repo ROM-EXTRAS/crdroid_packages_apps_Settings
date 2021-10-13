ResurrectionRemix/Resurrection_packages_apps_Settings

Navbar: Add button settings as an activity [2/2]
Fixes longpress navbar tile

Signed-off-by: Varun Date <date.varun123@gmail.com>

src/com/android/settings/Settings.java
@@ -192,4 +192,5 @@ public class Settings extends SettingsActivity {
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
     public static class DozeSettingsActivity extends SettingsActivity { /* empty */ }
     public static class ThemeSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class ButtonSettingsActivity extends SettingsActivity { /* empty */ }
 }

ROM-EXTRAS/crdroid_packages_apps_Settings

src/com/android/settings/Settings.java
@@ -247,4 +247,5 @@ public class Settings extends SettingsActivity {
     public static class SmartPixelsActivity extends SettingsActivity {}
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
     public static class GestureAnywhereSettings extends SettingsActivity {}
+    public static class NavbarSettingsActivity extends SettingsActivity { /* empty */ }
 }